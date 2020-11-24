834eb8978eecfec572664c101858885a883a9b30
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-11-24 17:33:50 +0300

    Add constants for mocks

---
+++ src/const.js
@@ -0,0 +1,36 @@
+export const TRIP_TYPES = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeing`, `Restaurant`];
+
+export const TRIP_DESTINATIONS = [`Geneva`, `London`, `St Petersburg`, `Berlin`, `Roma`, `Kiev`, `Paris`, `Budapest`, `Vienna`, `Milan`];
+
+export const OFFERS = [
+  {
+    id: 1,
+    title: `Add luggage`,
+    price: 50,
+  },
+  {
+    id: 2,
+    title: `Switch to comfort`,
+    price: 80,
+  },
+  {
+    id: 3,
+    title: `Add meal`,
+    price: 15,
+  },
+  {
+    id: 4,
+    title: `Choose seats`,
+    price: 5,
+  },
+  {
+    id: 5,
+    title: `Travel by train`,
+    price: 40,
+  },
+  {
+    id: 6,
+    title: `Order Uber`,
+    price: 20,
+  },
+];
