c13f68496bc16ebafdaaf5720f7e0cd4fe82b29e
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-11-26 10:28:28 +0300

    Add mock data to sort list template

--- src/view/sort-list.js
+++ src/view/sort-list.js
@@ -1,28 +1,20 @@
-export const getSortListTemplate = () => {
+const getSortItemsTemplate = (sortList) => {
+  return sortList.map(({title, isDisabled}, index) => {
+    const titleFormatted = title[0].toUpperCase() + title.slice(1);
+    const disabled = isDisabled ? `disabled` : ``;
+    const checked = index === 0 ? `checked` : ``;
+
+    return `<div class="trip-sort__item  trip-sort__item--${title}">
+              <input id="sort-${title}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${title}" ${checked} ${disabled}>
+              <label class="trip-sort__btn" for="sort-${title}">${titleFormatted}</label>
+            </div>`;
+  }).join(``);
+};
+
+export const getSortListTemplate = (sortList) => {
+  const sortTemplate = getSortItemsTemplate(sortList);
+
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-            <div class="trip-sort__item  trip-sort__item--day">
-              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
-              <label class="trip-sort__btn" for="sort-day">Day</label>
-            </div>
-
-            <div class="trip-sort__item  trip-sort__item--event">
-              <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
-              <label class="trip-sort__btn" for="sort-event">Event</label>
-            </div>
-
-            <div class="trip-sort__item  trip-sort__item--time">
-              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-              <label class="trip-sort__btn" for="sort-time">Time</label>
-            </div>
-
-            <div class="trip-sort__item  trip-sort__item--price">
-              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-              <label class="trip-sort__btn" for="sort-price">Price</label>
-            </div>
-
-            <div class="trip-sort__item  trip-sort__item--offer">
-              <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
-              <label class="trip-sort__btn" for="sort-offer">Offers</label>
-            </div>
+            ${sortTemplate}
           </form>`;
 };
