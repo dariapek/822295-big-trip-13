94a6033639231b7bf8a75d2c29aadd8a3619f2e2
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-11-26 10:26:56 +0300

    Add sort mock array

---
+++ src/mock/sort-mock.js
@@ -0,0 +1,24 @@
+export const getSortData = () => {
+  return [
+    {
+      title: `day`,
+      isDisabled: false,
+    },
+    {
+      title: `event`,
+      isDisabled: true,
+    },
+    {
+      title: `time`,
+      isDisabled: false,
+    },
+    {
+      title: `price`,
+      isDisabled: false,
+    },
+    {
+      title: `offer`,
+      isDisabled: true,
+    },
+  ];
+};
