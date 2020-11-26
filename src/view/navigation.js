85d2976ada8bc821a18686ec4f89e793c3310336
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-11-26 11:08:01 +0300

    Add navigation mock data to view component

--- src/view/navigation.js
+++ src/view/navigation.js
@@ -1,7 +1,19 @@
-export const getNavigation = () => {
+const getNavigationTemplate = (navigationData) => {
+  return navigationData.map(({title, isActive}) => {
+    const activeClass = isActive ? `trip-tabs__btn--active` : ``;
+
+    return `<a class="trip-tabs__btn  ${activeClass}" href="#">${title}</a>`;
+  }).join(``);
+};
+
+export const getNavigation = (navigationData) => {
+  console.log(navigationData);
+  const navigationTemplate = getNavigationTemplate(navigationData);
+
   return `<h2 class="visually-hidden">Switch trip view</h2>
             <nav class="trip-controls__trip-tabs  trip-tabs">
-              <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-              <a class="trip-tabs__btn" href="#">Stats</a>
+              ${navigationTemplate}
             </nav>`;
 };
+
+
