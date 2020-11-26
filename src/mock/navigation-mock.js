6334089d3bb721bf27da550cb9a86ff5046cfdf1
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-11-26 11:07:48 +0300

    Add navigation mock

---
+++ src/mock/navigation-mock.js
@@ -0,0 +1,12 @@
+export const getNavigationData = () => {
+  return [
+    {
+      title: `Table`,
+      isActive: true,
+    },
+    {
+      title: `Stats`,
+      isActive: false,
+    },
+  ];
+};
