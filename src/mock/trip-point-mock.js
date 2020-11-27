e4bf690dd2343ec142fb9263b110dec54ff5ceab
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-11-27 20:06:26 +0300

    Move description to const.js and change random generation

--- src/mock/trip-point-mock.js
+++ src/mock/trip-point-mock.js
@@ -2,18 +2,13 @@ import dayjs from "dayjs";
 
 import {getRandomInteger} from "../utils";
 
-import {OFFERS, TRIP_DESTINATIONS, TRIP_TYPES} from "../const";
+import {OFFERS, TRIP_DESTINATIONS, TRIP_TYPES, DESCRIPTIONS} from "../const";
 
 const getDescription = () => {
-  const descriptions = [
-    ``,
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget.`,
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra.`,
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus`,
-  ];
 
-  return descriptions[getRandomInteger(0, descriptions.length - 1)];
+  return DESCRIPTIONS
+    .splice(getRandomInteger(0, DESCRIPTIONS.length - 1), getRandomInteger(0, DESCRIPTIONS.length - 1))
+    .join(``);
 };
 
 const getPhotos = () => {
