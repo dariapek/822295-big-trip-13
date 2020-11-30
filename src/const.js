1d1060611244b01e54f91b251c0e03034651d9aa
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-11-30 22:16:44 +0300

    Fix description const

--- src/const.js
+++ src/const.js
@@ -31,7 +31,6 @@ export const OFFERS = [
 ];
 
 export const DESCRIPTIONS = [
-  ``,
   `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget.`,
   `Fusce tristique felis at fermentum pharetra.`,
   `Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
