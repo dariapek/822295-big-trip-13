6b623a364a7fe06117bfa726eb886cfe654b34a6
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-12-02 19:40:49 +0300

    Add class for Navigation

--- src/view/navigation.js
+++ src/view/navigation.js
@@ -1,3 +1,5 @@
+import {createElement} from "../utils";
+
 const getNavigationTemplate = (navigationData) => {
   return navigationData.map(({title, isActive}) => {
     const activeClass = isActive ? `trip-tabs__btn--active` : ``;
@@ -6,13 +8,38 @@ const getNavigationTemplate = (navigationData) => {
   }).join(``);
 };
 
-export const getNavigation = (navigationData) => {
+const getNavigation = (navigationData) => {
   const navigationTemplate = getNavigationTemplate(navigationData);
 
-  return `<h2 class="visually-hidden">Switch trip view</h2>
+  return `<div>
+            <h2 class="visually-hidden">Switch trip view</h2>
             <nav class="trip-controls__trip-tabs  trip-tabs">
               ${navigationTemplate}
-            </nav>`;
+            </nav>
+          </div>`;
 };
 
 
+export default class Navigation {
+  constructor(navigationData) {
+    this._navigationData = navigationData;
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    return getNavigation(this._navigationData);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
