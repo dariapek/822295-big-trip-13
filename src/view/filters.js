b02650161b0b998b6d7a565c244b7efa8ea4469c
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-12-02 19:40:03 +0300

    Add class for Filters

--- src/view/filters.js
+++ src/view/filters.js
@@ -1,3 +1,5 @@
+import {createElement} from "../utils";
+
 const getFiltersItemsTemplate = (filters) => {
   return filters.map((filterTitle, index) => {
     const checked = index === 0 ? `checked` : ``;
@@ -9,13 +11,38 @@ const getFiltersItemsTemplate = (filters) => {
   }).join(``);
 };
 
-export const getFiltersList = (filters) => {
+const getFiltersList = (filters) => {
   const filtersTemplate = getFiltersItemsTemplate(filters);
 
-  return `<h2 class="visually-hidden">Filter events</h2>
+  return `<div>
+            <h2 class="visually-hidden">Filter events</h2>
             <form class="trip-filters" action="#" method="get">
               ${filtersTemplate}
-
               <button class="visually-hidden" type="submit">Accept filter</button>
-            </form>`;
+            </form>
+          </div>`;
 };
+
+export default class Filters {
+  constructor(filters) {
+    this._filters = filters;
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    return getFiltersList(this._filters);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
