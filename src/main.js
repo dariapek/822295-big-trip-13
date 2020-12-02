14efc683b8a62fdcb65ab9dc928f8d251468e029
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-12-02 19:45:00 +0300

    Move functions to class and add edit view for point

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import {getInformationTemplate} from './view/information';
-import {getTotalPriceTemplate} from './view/total-price';
-import {getNavigation} from './view/navigation';
-import {getFiltersList} from './view/filters';
-import {getSortListTemplate} from './view/sort-list';
-import {getPointsListTemplate} from './view/points-list';
-import {getEditTemplate} from './view/edit-form';
-import {getPointTemplate} from './view/point';
+import InformationView from "./view/information";
+import TotalPriceView from "./view/total-price";
+import NavigationView from "./view/navigation";
+import FiltersView from "./view/filters";
+import SortView from "./view/sort";
+import PointsListView from "./view/points-list";
+import EditPointView from "./view/edit-form";
+import PointView from "./view/point";
 
-import {generateTripPoint} from './mock/trip-point-mock';
-import {getSortData} from './mock/sort-mock';
-import {getFilterData} from './mock/filters-mock';
-import {getNavigationData} from './mock/navigation-mock';
+import {generateTripPoint} from "./mock/trip-point-mock";
+import {getSortData} from "./mock/sort-mock";
+import {getFilterData} from "./mock/filters-mock";
+import {getNavigationData} from "./mock/navigation-mock";
+import {render, RenderPosition} from "./utils";
 
 const EVENT_COUNT = 15;
 
@@ -21,31 +22,54 @@ const tripEvents = body.querySelector(`.trip-events`);
 
 const tripPoints = new Array(EVENT_COUNT).fill().map(generateTripPoint);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
-render(main, getInformationTemplate(), `afterbegin`);
+render(main, new InformationView().getElement(), RenderPosition.AFTER_BEGIN);
 
 const tripInfo = body.querySelector(`.trip-info`);
 const sortData = getSortData();
 const filtersData = getFilterData();
 const navigationsData = getNavigationData();
 
-render(tripInfo, getTotalPriceTemplate(), `beforeend`);
-render(controls, getNavigation(navigationsData), `afterbegin`);
-render(controls, getFiltersList(filtersData), `beforeend`);
-render(tripEvents, getSortListTemplate(sortData), `beforeend`);
-render(tripEvents, getPointsListTemplate(), `beforeend`);
+render(tripInfo, new TotalPriceView().getElement(), RenderPosition.BEFORE_END);
+render(controls, new NavigationView(navigationsData).getElement(), RenderPosition.AFTER_BEGIN);
+render(controls, new FiltersView(filtersData).getElement(), RenderPosition.BEFORE_END);
+render(tripEvents, new SortView(sortData).getElement(), RenderPosition.BEFORE_END);
 
-const eventsList = tripEvents.querySelector(`.trip-events__list`);
+const renderPointsList = (container, points) => {
+  const pointsList = new PointsListView().getElement();
 
-const firstTripPointIndex = 0;
-render(eventsList, getEditTemplate(), `beforeend`);
-render(eventsList, getEditTemplate(tripPoints[firstTripPointIndex]), `beforeend`);
+  render(container, pointsList, RenderPosition.BEFORE_END);
 
-for (let i = firstTripPointIndex + 1; i < EVENT_COUNT; i++) {
-  render(eventsList, getPointTemplate(tripPoints[i]), `beforeend`);
-}
+  points.forEach((point) => {
+    renderPoint(pointsList, point);
+  });
+};
 
+const renderPoint = (pointListElement, point) => {
+  const pointComponent = new PointView(point);
+  const pointEditComponent = new EditPointView(point);
 
+  const replaceEditFormToPoint = () => {
+    pointListElement.replaceChild(pointComponent.getElement(), pointEditComponent.getElement());
+  };
+
+  const replacePointToPointEditForm = () => {
+    pointListElement.replaceChild(pointEditComponent.getElement(), pointComponent.getElement());
+  };
+
+  pointComponent.getElement()
+    .querySelector(`.event__rollup-btn`)
+    .addEventListener(`click`, () => {
+      replacePointToPointEditForm();
+    });
+
+  pointEditComponent.getElement()
+    .querySelector(`.event--edit`)
+    .addEventListener(`submit`, (evt) => {
+      evt.preventDefault();
+      replaceEditFormToPoint();
+    });
+
+  render(pointListElement, pointComponent.getElement(), RenderPosition.BEFORE_END);
+};
+
+renderPointsList(tripEvents, tripPoints);
