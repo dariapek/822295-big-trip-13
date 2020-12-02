fced79047f462c879eb679b0d4d10c1eff08a87a
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-12-02 19:40:20 +0300

    Add class for Information

--- src/view/information.js
+++ src/view/information.js
@@ -1,4 +1,6 @@
-export const getInformationTemplate = () => {
+import {createElement} from "../utils";
+
+const getInformationTemplate = () => {
   return `<section class="trip-main__trip-info  trip-info">
             <div class="trip-info__main">
               <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
@@ -7,3 +9,25 @@ export const getInformationTemplate = () => {
             </div>
           </section>`;
 };
+
+export default class Information {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return getInformationTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
