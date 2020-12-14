9c59382414caf86cb2f7c848908680a775f3190b
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2020-12-14 20:49:34 +0300

    Add point presenter

---
+++ src/presenter/point.js
@@ -0,0 +1,97 @@
+import PointView from "../view/point";
+import EditPointView from "../view/edit-form";
+import {render, RenderPosition, replace, remove} from "../utils/render";
+
+const Mode = {
+  DEFAULT: `DEFAULT`,
+  EDITING: `EDITING`
+};
+
+export default class Point {
+  constructor(pointsListContainer, changeData, changeMode) {
+    this._pointsListContainer = pointsListContainer;
+    this._changeData = changeData;
+    this._changeMode = changeMode;
+
+    this._pointComponent = null;
+    this._pointEditComponent = null;
+    this._mode = Mode.DEFAULT;
+
+    this._handleOpenClick = this._handleOpenClick.bind(this);
+    this._handleFormSubmit = this._handleFormSubmit.bind(this);
+    this._handleCloseClick = this._handleCloseClick.bind(this);
+    this._handleFavoriteClick = this._handleFavoriteClick.bind(this);
+  }
+
+  init(point) {
+    this._point = point;
+
+    const prevPointComponent = this._pointComponent;
+    const prevPointEditComponent = this._pointEditComponent;
+
+    this._pointComponent = new PointView(this._point);
+    this._pointEditComponent = new EditPointView(this._point);
+
+    this._pointComponent.setClickHandler(this._handleOpenClick);
+    this._pointComponent.setFavoriteClickHandler(this._handleFavoriteClick);
+    this._pointEditComponent.setFormSubmitHandler(this._handleFormSubmit);
+    this._pointEditComponent.setClickHandler(this._handleCloseClick);
+
+    if (prevPointComponent === null && prevPointEditComponent === null) {
+      render(this._pointsListContainer, this._pointComponent, RenderPosition.BEFORE_END);
+
+      return;
+    }
+
+    if (this._mode === Mode.DEFAULT) {
+      replace(this._pointComponent, prevPointComponent);
+    }
+
+    if (this._mode === Mode.EDITING) {
+      replace(this._pointEditComponent, prevPointEditComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevPointEditComponent);
+  }
+
+  destroy() {
+    remove(this._pointComponent);
+    remove(this._pointEditComponent);
+  }
+
+  resetView() {
+    if (this._mode !== Mode.DEFAULT) {
+      this._replaceFormToPoint();
+    }
+  }
+
+  _handleOpenClick() {
+    this._replaceFormToPoint();
+  }
+
+  _handleCloseClick() {
+    this._replacePointToForm();
+  }
+
+  _handleFormSubmit(evt) {
+    evt.preventDefault();
+    this._replaceFormToPoint();
+  }
+
+  _replacePointToForm() {
+    replace(this._pointEditComponent, this._pointComponent);
+    this._changeMode();
+    this._mode = Mode.EDITING;
+  }
+
+  _replaceFormToPoint() {
+    replace(this._pointComponent, this._pointEditComponent);
+    this._changeMode();
+    this._mode = Mode.DEFAULT;
+  }
+
+  _handleFavoriteClick() {
+    this._changeData(Object.assign({}, this._point, {isFavorite: !this._point.isFavorite}));
+  }
+}
