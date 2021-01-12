1a8aab9f4afaead266ba533d129e6066678186cb
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2021-01-12 19:52:54 +0300

    Refactor getOffersTemplate

--- src/view/edit-form.js
+++ src/view/edit-form.js
@@ -34,15 +34,25 @@ const getOffersTemplate = (offers, isCreateMode, checkedOffers) => {
     const offerTemplate = offers.map(({title, price}) => {
       const isChecked = checkedOffers.includes(title) ? `checked` : ``;
 
-    return `<div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="${id}" type="checkbox" name="${id}" ${isChecked || ``}>
-              <label class="event__offer-label" for="${id}">
+      return `<div class="event__offer-selector">
+              <input class="event__offer-checkbox  visually-hidden" id="${title}" type="checkbox" name="${title}" ${isChecked}>
+              <label class="event__offer-label" for="${title}">
                 <span class="event__offer-title">${title}</span>
-                &plus;&euro;&nbsp;
+                 &plus;&euro;&nbsp;
                 <span class="event__offer-price">${price}</span>
               </label>
             </div>`;
-  }).join(``);
+    });
+
+    return `<section class="event__section  event__section--offers">
+              <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+              <div class="event__available-offers">
+                ${offerTemplate.join(``)}
+              </div>
+            </section>`;
+  } else {
+    return ``;
+  }
 };
 
 const getEventTypeListItemTemplate = (tripTypes) => {
@@ -138,13 +148,7 @@ const getEditTemplate = (pointData) => {
                   </button>
                 </header>
                 <section class="event__details">
-                  <section class="event__section  event__section--offers">
-                    <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
-                    <div class="event__available-offers">
-                      ${offersTemplate}
-                    </div>
-                  </section>
+                  ${offersTemplate}
                   ${destinationSectionTemplate}
               </form>
             </li>`;
