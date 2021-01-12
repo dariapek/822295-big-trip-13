941cb755ce4f2bf01722d1e8c412744bf28c36ff
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2021-01-12 19:45:23 +0300

    Remove unused constant OFFERS

--- src/const.js
+++ src/const.js
@@ -2,33 +2,6 @@ export const TRIP_TYPES = [`taxi`, `bus`, `train`, `ship`, `transport`, `drive`,
 
 export const TRIP_DESTINATIONS = [`Geneva`, `London`, `St Petersburg`, `Berlin`, `Roma`, `Kiev`, `Paris`, `Budapest`, `Vienna`, `Milan`, `Chamonix`];
 
-export const OFFERS = [
-  {
-    id: `event-offer-luggage-1`,
-    title: `Add luggage`,
-    price: 50,
-  },
-  {
-    id: `event-offer-comfort-1`,
-    title: `Switch to comfort`,
-    price: 80,
-  },
-  {
-    id: `event-offer-meal-1`,
-    title: `Add meal`,
-    price: 15,
-  },
-  {
-    id: `event-offer-seats-1`,
-    title: `Choose seats`,
-    price: 5,
-  },
-  {
-    id: `event-offer-train-1`,
-    title: `Travel by train`,
-    price: 40,
-  },
-];
 
 export const DESCRIPTIONS = [
   `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget.`,
