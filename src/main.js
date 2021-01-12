ff4192a55e874948cf67213311dca4d1e74ee2fa
Daria Pekhtereva <d.pekhtereva@ndmsystems.com>
2021-01-12 19:44:59 +0300

    Add offers to trip presenter

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import TripPresenter from "./presenter/trip";
 import {generateTripPoint} from "./mock/trip-point-mock";
 import {getFilterData} from "./mock/filters-mock";
 import {getNavigationData} from "./mock/navigation-mock";
+import {getOfferMockData} from "./mock/offers-mock";
 import {render, RenderPosition} from "./utils/render";
 
 const EVENT_COUNT = 15;
@@ -23,12 +24,13 @@ render(main, new InformationView(), RenderPosition.AFTER_BEGIN);
 const tripInfo = body.querySelector(`.trip-info`);
 const filtersData = getFilterData();
 const navigationsData = getNavigationData();
+const offers = getOfferMockData();
 
 render(tripInfo, new TotalPriceView(), RenderPosition.BEFORE_END);
 render(controls, new NavigationView(navigationsData), RenderPosition.AFTER_BEGIN);
 render(controls, new FiltersView(filtersData), RenderPosition.BEFORE_END);
 
 
-const tripPresenter = new TripPresenter(tripEvents);
+const tripPresenter = new TripPresenter(tripEvents, offers);
 tripPresenter.init(tripPoints);
 
